6671d8ccfedbea8f82cf37faa142a7e5589a29e3
francescoferraioli <francesco.ferraioli@connect.qut.edu.au>
2014-04-29 09:45:15 +1000

    did some stuff

---
+++ INB373/Assignment/BudgetManage/BudgetManage/BudgetManage/index.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/BudgetManage.Master" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="BudgetManage.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <style type="text/css">
+    #TextArea1 {
+        height: 237px;
+        width: 358px;
+    }
+</style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <p>
+        &nbsp;Some random text which means nothing.</p>
+<p>
+    <textarea id="TextArea1" name="S1"></textarea></p>
+<p>
+    <asp:Button ID="Button1" runat="server" Text="Button" Width="128px" />
+</p>
+</asp:Content>
---
+++ INB373/Assignment/Business/Items.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Business
+{
+    [DataObject]
+    public class Item
+    {
+        private static DataAccess.SouthwindTableAdapters.ItemsTableAdapter adapter = new DataAccess.SouthwindTableAdapters.ItemsTableAdapter();
+        [DataObjectMethod(DataObjectMethodType.Update)]
+        public static void UpdateItem(long original_ItemId, string name, decimal amount, bool income, datetime date)
+        {
+            adapter.Update(name, amount, income, date, original_ItemId);
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Insert)]
+        public static void InsertItem(long budgetId, string name, decimal amount, bool income, datetime data)
+        {
+            adapter.Insert(budgetId, name, amount, income, date);
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Delete)]
+        public static void DeleteItem(long original_ItemId)
+        {
+            adapter.Delete(original_ItemId);
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select)]
+        public static Data.Southwind.ItemsDataTable GetItem(long budgetId)
+        {
+            return adapter.GetData(budgetId);
+
+        }
+
+    }
+}
